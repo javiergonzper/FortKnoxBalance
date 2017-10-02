ced41a946402d660d4126cf718f9da66e917ca9c
Javier Gonzalez <javiergonzper@gmail.com>
2017-10-02 11:09:08 +0200

    Initial commit

---
+++ Fort Knox Balance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fort Knox Balance
+//
+//  Created by Javier Gonzalez Perez on 2/10/17.
+//  Copyright © 2017 Treasire Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
